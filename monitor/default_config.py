7d78892f33d239089fc2c8432d81a35e60f5b9ff
chexinze <chexinze@bytedance.com>
2020-01-15 23:14:58 +0800

    feature: get and set config

--- monitor/default_config.py
+++ monitor/default_config.py
@@ -1,5 +1,3 @@
-
-
 # spark default config
 
 class Config():
@@ -14,14 +12,30 @@ class Config():
             for line in lines:
                 if not line.strip() or line[0] != '#':
                     cf = line.split(' ')
-                    if len(cf)==2:
+                    if len(cf) == 2:
                         self.conf[cf[0]] = cf[1][:-1]
+
+    def get(self, conf_key):
+        return self.conf[conf_key]
+
+    def set(self, conf_key, conf_value):
+        self.conf[conf_key] = conf_value
+
+    def get_all(self):
+        for key in self.conf.keys():
+            print('--config ' + key + ' ' + str(self.conf[key]))
+
+    def get_part(self, part_name):
+        for key in self.conf.keys():
+            if part_name in key:
+                print('--config ' + key + ' ' + str(self.conf[key]))
 
 
 if __name__ == '__main__':
-
     config = Config()
     config._read_from_file()
     # print(config.conf)
     print(config.conf['spark.port.maxRetries'])
     print('total config parameters: ' + str(len(config.conf)))
+    # config.get_all()
+    config.get_part('shuffle')
